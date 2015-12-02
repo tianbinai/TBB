f1e0ab94048dd7f0fb8c7417f491187f6e961b81
qianfeng <qianfeng@bajiepaoliangquan.local>
2015-12-02 20:12:02 +0800

    aaaaaaaaaaa

---
+++ TBBBBBBB/TBBBBBBB/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TBBBBBBB
+//
+//  Created by qianfeng on 15/12/2.
+//  Copyright (c) 2015年 TB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TBBBBBBB/TBBBBBBB/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  TBBBBBBB
+//
+//  Created by qianfeng on 15/12/2.
+//  Copyright (c) 2015年 TB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+-(void)sdnoadiosapdoapsd;
+
+@end
+
